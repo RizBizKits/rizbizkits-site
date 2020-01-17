23947f557870aae735a9c5f59c863eb4c011b197
Riz <rizakmalkhan@gmail.com>
2020-01-17 22:15:34 +0000

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,39 +6,49 @@ module.exports = {
 		siteUrl: 'https://rizwanakhan.com/'
 	},
 	// previous setup
-	  plugins: [
-	  'gatsby-plugin-catch-links',
-	  'gatsby-plugin-react-helmet',
-	  'gatsby-plugin-feed',
-	  {
-		resolve: `gatsby-source-filesystem`,
-		options: {
-		  path: `${__dirname}/src/pages`,
-		  name: 'pages',
-		},
-	  },
-
-	  {
-		resolve: 'gatsby-plugin-matomo',
-		options: {
-			siteId: '13',
-			matomoUrl: 'https://stats.jvt.me',
-			siteUrl: 'https://rizwanakhan.com/'
-		},
+	plugins: [
+		'gatsby-plugin-catch-links',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-feed',
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/src/pages`,
+				name: 'pages',
+			},
 		},
 
-	  {
-		resolve: 'gatsby-transformer-remark',
-		options: {
-		  plugins: [] // just in case those previously mentioned remark plugins sound cool :)
-		}
-	  },
-	  {
-		resolve: `gatsby-transformer-remark`,
-		options: {
-		  plugins: [`gatsby-remark-responsive-iframe`, `gatsby-remark-social-cards`],
-		  
+		{
+			resolve: 'gatsby-plugin-matomo',
+			options: {
+				siteId: '13',
+				matomoUrl: 'https://stats.jvt.me',
+				siteUrl: 'https://rizwanakhan.com/'
+			},
 		},
-	  },
+
+
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				icon: `static/rizbizkits-logo.png`, // This path is relative to the root of the site.
+			}
+		},
+
+
+		{
+			resolve: 'gatsby-transformer-remark',
+			options: {
+				plugins: [] // just in case those previously mentioned remark plugins sound cool :)
+			}
+		},
+
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [`gatsby-remark-responsive-iframe`, `gatsby-remark-social-cards`],
+
+			},
+		},
 	]
-  }
+}
