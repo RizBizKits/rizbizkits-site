137eb327021985a95376fe320a69c92106e3e05c
Riz <rizakmalkhan@gmail.com>
2020-04-22 19:17:08 +0100

    NOJ - 21.04.20

--- src/pages/nuggetofjoy.js
+++ src/pages/nuggetofjoy.js
@@ -16,18 +16,15 @@ export default () => (
     </div>
 
 <div class="joy-outer">
-
-
-
     <div class="joy-inner">
 
         <div class="joy-i">
             <div class="joy-content">
-                <p class="joy-date"><span class="font-yellow">20.</span> <span class="font-purple">04.</span> <span class="font-clear">2020</span></p>
+                <p class="joy-date"><span class="font-yellow">21.</span> <span class="font-purple">04.</span> <span class="font-clear">2020</span></p>
                 <p>
-                <strong>Today</strong> - Further in our shift from 🏠 to a bakery, 'cuz we'd eaten all the 🍊 cake, mom baked a coffee cake!
+                <strong>Today</strong> - Fulfilling quiz master duties 👀 Laughing over the incorrect answers & flexing on the right ones; joy as a collective 💛
                 </p>
-                <p>thus, thou the top was a little burnt, the joy of having the company of loved ones to share the joy of the cake with was so great, it was palpable.</p>
+                {/* <p>thus, thou the top was a little burnt, the joy of having the company of loved ones to share the joy of the cake with was so great, it was palpable.</p> */}
             </div>
 
             <div class="joy-image">
